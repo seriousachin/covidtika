0f97313c405942c2e0ccdbaf28e648e892285cfc
Sachin <serioussachin@hotmail.com>
2021-06-03 00:53:07 +0530

    piechart

--- app.py
+++ app.py
@@ -195,15 +195,18 @@ ad.append({'Last 7 days daily avg':'Between 1000-2000',
      'No of Districts':df[df['avgdaily']<2000 ].count()[1]-df[df['avgdaily']<=1000].count()[1]})
 ad.append({'Last 7 days daily avg':'Between 2000-5000',
      'No of Districts':df[df['avgdaily']<5000 ].count()[1]-df[df['avgdaily']<=2000].count()[1]})
-ad.append({'Last 7 days daily avg':'More than 5000',
-     'No of Districts':df[df['avgdaily']>=5000 ].count()[1]})
+ad.append({'Last 7 days daily avg':'Between 5000-10000',
+     'No of Districts':df[df['avgdaily']<10000 ].count()[1]-df[df['avgdaily']<=5000].count()[1]})
+ad.append({'Last 7 days daily avg':'More than 10000',
+     'No of Districts':df[df['avgdaily']>=10000 ].count()[1]})
 ad=pd.DataFrame(ad)
 fig = px.pie(ad,values='No of Districts', names='Last 7 days daily avg',title='No of Districts: Last 7 days daily avg vaccinated people(45+ group)',color='Last 7 days daily avg', color_discrete_map={'Less than 200':'RGB(153,0,0)',
                                  'Between 200-500':'RGB(255,51,51)',
                                  'Between 500-1000':'RGB(255,153,153)',
                                  'Between 1000-2000':'RGB(255,204,204)',
                                  'Between 2000-5000':'RGB(229,255,204)',
-                                 'More than 5000':'RGB(0,102,0)'})
+                                 'Between 5000-10000':'RGB(0,255,0)',
+                                 'More than 10000':'RGB(0,102,0)'})
 fig.update_traces(hoverinfo='label+percent', textinfo='value')
 fig.add_annotation(text = 'Sachin Pandey @serioussachin, https://covidtika.herokuapp.com & CoWIN, 2 June',
                               font_size = 10,
@@ -222,8 +225,10 @@ ad18.append({'Last 7 days daily avg':'Between 1000-2000',
      'No of Districts':df[df['avgdaily 18-44']<2000 ].count()[1]-df[df['avgdaily 18-44']<=1000].count()[1]})
 ad18.append({'Last 7 days daily avg':'Between 2000-5000',
      'No of Districts':df[df['avgdaily 18-44']<5000 ].count()[1]-df[df['avgdaily 18-44']<=2000].count()[1]})
-ad18.append({'Last 7 days daily avg':'More than 5000',
-     'No of Districts':df[df['avgdaily 18-44']>=5000 ].count()[1]})
+ad18.append({'Last 7 days daily avg':'Between 5000-10000',
+     'No of Districts':df[df['avgdaily 18-44']<10000 ].count()[1]-df[df['avgdaily 18-44']<=5000].count()[1]})
+ad18.append({'Last 7 days daily avg':'More than 10000',
+     'No of Districts':df[df['avgdaily 18-44']>=10000 ].count()[1]})
 ad18=pd.DataFrame(ad18)
 #fig = px.bar(ad, x='Last 7 days daily avg', y='No of Districts')
 #fig.update_traces(textposition='outside')
@@ -233,7 +238,8 @@ fig18 = px.pie(ad18,values='No of Districts', names='Last 7 days daily avg',titl
                                  'Between 500-1000':'RGB(255,153,153)',
                                  'Between 1000-2000':'RGB(255,204,204)',
                                  'Between 2000-5000':'RGB(229,255,204)',
-                                 'More than 5000':'RGB(0,102,0)'})
+                                 'Between 5000-10000':'RGB(0,255,0)',
+                                 'More than 10000':'RGB(0,102,0)'})
 fig18.update_traces(hoverinfo='label+percent', textinfo='value')
 fig18.add_annotation(text = 'Sachin Pandey @serioussachin, https://covidtika.herokuapp.com & CoWIN, 2 June',
                               font_size = 10,
