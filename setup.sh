4d515dbfcc1533bc838ec1446725443a727f2b54
Sachin <serioussachin@hotmail.com>
2021-06-10 15:58:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+file=~/.streamlit/static/index.html
+sed -i 's/"<title>Streamlit</title>"/"<title>Covid Tika Tracker</title>"/g' $file
