784f6494d4d28d189327f50260cac3e3fb9bae9c
Sachin <serioussachin@hotmail.com>
2021-07-21 07:43:28 +0530

    20july

--- footer_utils.py
+++ footer_utils.py
@@ -56,7 +56,7 @@ def layout(*args):
 
 def footer(inp_views):
     myargs = [
-        "views- ", str(inp_views +20581),
+        "views- ", str(inp_views +20671),
         ". Created by  ",
         link("https://twitter.com/serioussachin", "Sachin Pandey"),"   ","  ",
         
