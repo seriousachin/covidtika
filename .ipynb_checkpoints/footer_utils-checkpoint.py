733520954503a356c4e68eb7022ce4c03f0d0452
seriousachin <84611634+seriousachin@users.noreply.github.com>
2021-11-12 21:04:12 +0530

    11nov

--- .ipynb_checkpoints/footer_utils-checkpoint.py
+++ .ipynb_checkpoints/footer_utils-checkpoint.py
@@ -56,10 +56,9 @@ def layout(*args):
 
 def footer(inp_views):
     myargs = [
-        "views- ", str(inp_views +16640),
+        "views- ", str(inp_views +33976),
         ". Created by  ",
         link("https://twitter.com/serioussachin", "Sachin Pandey"),"   ","  ",
-        link("upi://pay?pa=sachinpandey@apl&pn=SachinPandey&cu=INR","Buy me  a cup of tea? upi: sachinpandey@apl"), " "
         
     ]
     layout(*myargs)
--- footer_utils.py
+++ footer_utils.py
@@ -56,7 +56,7 @@ def layout(*args):
 
 def footer(inp_views):
     myargs = [
-        "views- ", str(inp_views +21976),
+        "views- ", str(inp_views +33976),
         ". Created by  ",
         link("https://twitter.com/serioussachin", "Sachin Pandey"),"   ","  ",
         
